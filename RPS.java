7ec5942bae910c9644af421b4434072282b15cdd
Raha Pirzadeh <pirzadeh@chapman.edu>
2020-04-22 13:07:24 -0700

    Add files via upload

--- RPS.java
+++ RPS.java
@@ -6,74 +6,87 @@ public class RPS {
 	public static final String PAPER = "P";
 	public static final String SCISSORS = "S";
 	public static final String NO ="N";
+	public static final String YES ="Y";
+	static int player1wins = 0;
+	static int player2wins = 0;
+
+	// Main method
+	public static void main(String[] args) {
+		rps();
+	}
 
 	//Get the game Result
 	public static void getResult(String user1Move, String user2Move) {
+		//int player1wins=0;
 
-        if (user1Move.equals(user2Move))
-	        System.out.println("It's a tie!\n");
+		if (user1Move.equals(user2Move))
+	  	System.out.println("It's a tie!\n");
 
-        else if (user1Move.equals(ROCK))
-        {
-	        if (user2Move.equals(SCISSORS))
-		        System.out.println("Player 1 Wins!!\n"+"Player 2 Looses...\n"+"Rock crushes scissors.\n");
-	        else if (user2Move.equals(PAPER))
-		        System.out.println("Player 2 Wins!!\n"+"Player 1 Looses...\n"+"Paper eats rock.\n");
-					else
-						System.out.println("Invalid input player 2.\n");
-        }
-        else if (user1Move.equals(PAPER))
-        {
-            if (user2Move.equals(ROCK))
-		        System.out.println("Player 1 Wins!!\n"+"Player 2 Looses...\n"+"Paper eats rock.\n");
-	        else if (user2Move.equals(SCISSORS))
-		        System.out.println("Player 2 Wins!!\n"+"Player 1 Looses...\n"+"Scissor cuts paper.\n");
-					else
-						System.out.println("Invalid input player 2.\n");
-        }
-        else if (user1Move.equals(SCISSORS))
-        {
-	        if (user2Move.equals(PAPER))
-		        System.out.println("Player 1 Wins!!\n"+"Player 2 Looses...\n"+"Scissor cuts paper.\n");
-	        else if (user2Move.equals(ROCK))
-		        System.out.println("Player 2 Wins!!\n"+"Player 1 Looses...\n"+"Rock crushes scissors.\n");
-					else
-						System.out.println("Invalid input player 2.\n");
-        }
-        else
-    	    System.out.println("Invalid input player 1.\n");
+    else if (user1Move.equals(ROCK)) {
+			if (user2Move.equals(SCISSORS))
+				System.out.println(++player1wins + "Player 1 Wins!!\n"+"Player 2 Looses...\n"+"Rock crushes scissors.\n");
+	    else if (user2Move.equals(PAPER))
+		    System.out.println(player2wins++ + "Player 2 Wins!!\n"+"Player 1 Looses...\n"+"Paper eats rock.\n");
+			else
+				System.out.println("Invalid input player 2.\n");
+		}
+		else if (user1Move.equals(PAPER)) {
+    	if (user2Move.equals(ROCK))
+		  	System.out.println(++player1wins + "Player 1 Wins!!\n"+"Player 2 Looses...\n"+"Paper eats rock.\n");
+				//player1wins++;
+	    else if (user2Move.equals(SCISSORS))
+		    System.out.println(player2wins++ + "Player 2 Wins!!\n"+"Player 1 Looses...\n"+"Scissor cuts paper.\n");
+				//player2wins++;
+			else
+				System.out.println("Invalid input player 2.\n");
     }
+    else if (user1Move.equals(SCISSORS)) {
+	  	if (user2Move.equals(PAPER))
+		  	System.out.println(++player1wins + "Player 1 Wins!!\n"+"Player 2 Looses...\n"+"Scissor cuts paper.\n");
+				//player1wins++;
+	    else if (user2Move.equals(ROCK))
+		  	System.out.println(player2wins++ + "Player 2 Wins!!\n"+"Player 1 Looses...\n"+"Rock crushes scissors.\n");
+				//player2wins++;
+			else
+				System.out.println("Invalid input player 2.\n");
+    }
+    else
+    	System.out.println("Invalid input player 1.\n");
+  }
 
 	// User's move
 	public static String getUser1Move(){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("\nPlayer 1 choice: ");
-	String input = scanner.next().toUpperCase();
+		String input = scanner.next().toUpperCase();
 			return input;
-
 	}
 
 	public static String getUser2Move(){
-	    Scanner scanner = new Scanner(System.in);
-	    System.out.println("Player 2 choice: ");
+		Scanner scanner = new Scanner(System.in);
+	  System.out.println("Player 2 choice: ");
 		String input = scanner.next().toUpperCase();
-        return input;
-    }
+    	return input;
+  }
 
-    // Main method
-	public static void main(String[] args) {
+	//rps menu
+	public static void rps(){
 		System.out.println( "Rock, Paper, Scissors!\n"
 				+ "Please enter a move.\n"
 				+"Rock = R, Paper= P, and Scissors = S\n");
-			do {
-				getResult(getUser1Move(), getUser2Move());
-				System.out.println("---Want to play again? Y/N---");
-				Scanner scanner = new Scanner(System.in);
-				String play = scanner.next().toUpperCase();
-				if(play.equals(NO)){
-					break;
-				}
-
-			} while (true);
+		do {
+			getResult(getUser1Move(), getUser2Move());
+			System.out.println("Player 1 scores: "+ player1wins);
+			System.out.println("Player 2 scores: "+ player2wins);
+			System.out.println("---Want to play again? Y/N---");
+			Scanner scanner = new Scanner(System.in);
+			String play = scanner.next().toUpperCase();
+			if(play.equals(NO)) {
+				break;
+			}
+			else if (!play.equals(YES)) {
+				System.out.println("Incorrect choice");
+			}
+		} while (true);
 	}
 }
