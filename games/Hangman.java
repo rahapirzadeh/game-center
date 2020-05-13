aaf5b2e9ad7721ce799f6862bc4b62475dd6809d
Brandon Fabre <fabr@chapman.edu>
2020-05-12 20:12:17 -0700

    Updates javadocs formatting

--- games/Hangman.java
+++ games/Hangman.java
@@ -36,7 +36,9 @@ public class Hangman extends Game {
   private char[] correctlyGuessedLetters;
   private boolean firstGame = true;
 
-  /** method to pass in one player.*/
+  /**
+   * Constructs a new single-player game instance of Hangman.
+   */
   public Hangman() {
     super();
     selectWordToGuess();
@@ -44,7 +46,13 @@ public class Hangman extends Game {
     Arrays.fill(correctlyGuessedLetters, '_'); // Indicates no correctly guessed letters
   }
 
-  /** method to pass in both players.*/
+  /**
+   * Constructs a new dual-player game instance of Hangman using {@code p1} and {@code p2}.
+   * @param p1
+   *        Player 1
+   * @param p2
+   *        Player 2
+   */
   public Hangman(Player p1, Player p2) {
     super(p1, p2);
     selectWordToGuess();
@@ -52,7 +60,9 @@ public class Hangman extends Game {
     Arrays.fill(correctlyGuessedLetters, '_'); // Indicates no correctly guessed letters
   }
 
-  /** runs hangman.*/
+  /**
+   * Starts instance of Hangman game.
+   */
   public void run() {
     if (firstGame) {
       printWelcomeMessage();
@@ -74,138 +84,249 @@ public class Hangman extends Game {
     }
   }
 
-  /** selects random word from textfile.*/
+  /**
+   * Sets guess word to word selected by getting random line from hangmanWords.txt.
+   */
   public void selectWordToGuess() {
     int wordIndex = new Random().nextInt(19); //19 is the # of words listed in ./hangmanWords.txt
     wordToGuess = FileIO.getLineFromFile("hangmanWords.txt", wordIndex);
   }
 
-  /** resets winstreak when player looses.*/
+  /**
+   * Resets win streak of player to 0.
+   */
   public void resetWinStreak() {
     winStreak = 0;
   }
 
-  /** resets guesses when a round is finished.*/
+  /**
+   * Resets number of total, incorrect, and correct guesses when round finishes.
+   */
   public void resetGuesses() {
     numRoundTotalGuesses = numRoundIncorrectGuesses = numRoundCorrectGuesses = 0;
   }
 
-  /** resets the board to "-" when a round is finished.*/
+  /**
+   * Resets {@code correctlyGuessedLetters} to all underscores, indicating no letters have been correctly guessed.
+   */
   public void resetCorrectlyGuessedLetters() {
     correctlyGuessedLetters = new char[wordToGuess.length()];
     Arrays.fill(correctlyGuessedLetters, '_');
   }
 
-  /** increments the win streak.*/
+  /**
+   * Increments both players' win streaks by 1.
+   */
   public void incrementWinStreak() {
     winStreak++;
   }
 
-  /** increments the number of rounds played.*/
+  /**
+   * Increments number of rounds played in the current game by 1.
+   */
   public void incrementNumRoundsPlayed() {
     numRoundsPlayed++;
   }
 
-  /** increments the number of rounds won.*/
+  /**
+   * Increments number of rounds won in the current game by 1.
+   */
   public void incrementNumRoundsWon() {
     numRoundsWon++;
   }
 
-  /** increments the number of rounds lost.*/
+  /**
+   * Increments number of rounds lost in the current game by 1.
+   */
   public void incrementNumRoundsLost() {
     numRoundsLost++;
   }
 
-  /** increments the number of guesses in the round.*/
+  /**
+   * Increments number of total guesses in the current game by 1.
+   */
   public void incrementNumTotalGuesses() {
     numRoundTotalGuesses++;
   }
 
-  /** increments the number of incorrect guesses in the round.*/
+  /**
+   * Increments number of incorrect guesses in the current game by 1.
+   */
   public void incrementNumIncorrectGuesses() {
     numRoundIncorrectGuesses++;
   }
 
-  /** increments the number of correct guesses in the round.*/
+  /**
+   * Increments number of correct guesses in the current game by 1.
+   */
   public void incrementNumCorrectGuesses() {
     numRoundCorrectGuesses++;
   }
 
-  /** increments the number of points.*/
+  /**
+   * Increments both players' points by 1.
+   */
   public void incrementPoints() {
     points++;
   }
 
-  /** method to get the guess from current player.*/
-  public void setCurrGuess(String newGuess) {
-    currGuess = newGuess;
+  /**
+   * Sets current guess.
+   * @param guess
+   *        Player's current guess
+   */
+  public void setCurrGuess(String guess) {
+    this.currGuess = guess;
   }
 
-  /** method to return the guess from current player.*/
+  /**
+   * Gets current guess.
+   * @return  current guess
+   */
   public String getCurrGuess() {
     return currGuess;
   }
 
-  /** method to return win streak.*/
+  /**
+   * Returns number of consecutive wins.
+   * @return  number of consecutive wins
+   */
   public int getWinStreak() {
     return winStreak;
   }
 
-  /** method to return word to guess.*/
+  /**
+   * Returns current guess word.
+   * @return  current guess word
+   */
   public String getWordToGuess() {
     return wordToGuess;
   }
 
-  /** method to return number of correctly guessed letters.*/
+  /**
+   * Returns character array of letters that have already been guessed which appear in the guess word.
+   * @return  the array of characters correctly guessed by the player(s)
+   */
   public char[] getCorrectlyGuessedLetters() {
     return correctlyGuessedLetters;
   }
 
-  /** convertStringToUnderscores() converts unguessed letters in guessWord to underscores
-      charArrayToString() converts correct guesses to string form for display purposes
-      replaceAll("\\B", " ") adds spaces after each character.*/
+  /**
+   * Returns progress of guess word, with underscores indicating unguessed letters.Displays progress of guess word to player(s)
+   *
+   * Converts guesses to string format and adds space after each letter in string for improved readability.
+   *
+   * Examples of guess word progress print statements are in the following table.
+   *
+   * <table class="plain">
+   * <thead>
+   * <tr>
+   *   <th scope="col">Print Result</th>
+   *   <th scope="col">Guess Word</th>
+   *   <th scope="col">Letters Guessed</th>
+   *   <th scope="col">Outstanding Letters</th>
+   * </tr>
+   * </thead>
+   * <tbody>
+   * <tr>
+   *   <td>d _ _ _</td>
+   *   <td>data</td>
+   *   <td>d</td>
+   *   <td>a,t</td>
+   * </tr>
+   * <tr>
+   *   <td>p o l y m o r p h _ _ m</td>
+   *   <td>polymorphism</td>
+   *   <td>p,o,l,y,m,h</td>
+   *   <td>i,s</td>
+   * </tr>
+   * <tr>
+   *   <td>j a v a</td>
+   *   <td>java</td>
+   *   <td>j,v,a</td>
+   *   <td>(none)</td>
+   * </tr>
+   * <tr>
+   *   <td>b _ o _ k _ h _ i n</td>
+   *   <td>blockchain</td>
+   *   <td>b,o,k,h,i</td>
+   *   <td>l,c,a</td>
+   * </tr>
+   * </tbody>
+   * </table>
+   *
+   * @return  progress of guess word in string format, with underscores indicating unguessed letters
+   */
   public String getGuessWordProgress() {
     return StringManipulation.convertStringToUnderscores(
-            StringManipulation.charArrayToString(getCorrectlyGuessedLetters()))
-            .replaceAll("\\B", " ");
+            StringManipulation.charArrayToString(getCorrectlyGuessedLetters())) // Converts guesses to string form
+            .replaceAll("\\B", " "); // Adds space after each letter for improved readability
   }
 
-  /** method to return number of guesses at end of round.*/
+  /**
+   * Returns number of total guesses.
+   * @return  number of total guesses
+   */
   public int getNumRoundTotalGuesses() {
     return numRoundTotalGuesses;
   }
 
-  /** method to return number of correct guesses at end of round.*/
+  /**
+   * Returns number of correct guesses.
+   * @return  number of correct guesses
+   */
   public int getNumRoundCorrectGuesses() {
     return numRoundCorrectGuesses;
   }
 
-  /** method to return number of incorrect guesses at end of round.*/
+  /**
+   * Returns number of incorrect guesses.
+   * @return  number of incorrect guesses
+   */
   public int getNumRoundIncorrectGuesses() {
     return numRoundIncorrectGuesses;
   }
 
-  /** method to return number of rounds played.*/
+  /**
+   * Returns number of rounds played.
+   * @return  number of rounds played
+   */
   public int getNumRoundsPlayed() {
     return numRoundsPlayed;
   }
 
-  /** method to return numberof rounds won at end of game.*/
+  /**
+   * Returns number of rounds won.
+   * @return  number of rounds won
+   */
   public int getNumRoundsWon() {
     return numRoundsWon;
   }
 
-  /** method to return number of rounds lost at end of game.*/
+  /**
+   * Returns number of rounds lost.
+   * @return  number of rounds lost
+   */
   public int getNumRoundsLost() {
     return numRoundsLost;
   }
 
-  /** method to return points at end of round.*/
+  /**
+   * Returns number points accumulated by player(s).
+   * @return  number of points accumulated by player(s)
+   */
   public int getPoints() {
     return points;
   }
 
-  /** method to check if guess is correct.*/
+  /**
+   * Determines whether {@code guess} is a correct guess
+   *
+   * A correct guess can be a character that appears in the guess word, multiple characters
+   * separated by commas that appear in the guess word, or the entire guess word itself
+   * @param guess
+   *        the player's current guess
+   */
   public void checkIfGuessCorrect(String guess) {
     if (guess.length() > 1) {
       if (guess.contains(",")) {
@@ -224,7 +345,16 @@ public class Hangman extends Game {
     }
   }
 
-  /**  method to place correct guess on board.*/
+  /**
+   * Assert whether a character guess is correct.
+   *
+   * If the guess is correct, {@link Hangman#correctGuess(char)} is called. Else, {@link Hangman#incorrectGuess()} is called.
+   *
+   * A character guess is considered correct if it appears in the guess word and has not
+   * already been guessed.
+   * @param c
+   *        the character guessed
+   */
   public void assertCorrectGuess(char c) {
     if (characterInGuessWord(c) && guessNotInPreviousGuesses(c)) {
       correctGuess(c);
@@ -233,7 +363,15 @@ public class Hangman extends Game {
     }
   }
 
-  /**  method to place correct guess on board when given as string.*/
+  /**
+   * Assert whether a string guess is correct.
+   *
+   * If the guess is correct, {@link Hangman#correctGuess(String)} is called. Else, {@link Hangman#incorrectGuess()} is called.
+   *
+   * A character guess is considered correct if it is equal to the guess word, ignoring case.
+   * @param s
+   *        the string guessed
+   */
   public void assertCorrectGuess(String s) {
     if (getWordToGuess().equalsIgnoreCase(s)) {
       correctGuess(s);
@@ -242,12 +380,22 @@ public class Hangman extends Game {
     }
   }
 
-  /** method to get how many letters in word to place on board.*/
+  /**
+   * Returns whether a character guessed appears in the guess word.
+   * @param c
+   *        the character guessed
+   * @return  {@code true} if the character appears in guess word; {@code false} otherwise
+   */
   public boolean characterInGuessWord(char c) {
     return getWordToGuess().indexOf(c) != -1;
   }
 
-  /** method to check if letter was guessed before.*/
+  /**
+   * Returns whether a character guessed has already been guessed.
+   * @param c
+   *        the character guessed
+   * @return  {@code true} if character was not previously guessed; {@code false} otherwise
+   */
   public boolean guessNotInPreviousGuesses(char c) {
     for (char character : correctlyGuessedLetters) {
       if (character == c) {
@@ -257,7 +405,11 @@ public class Hangman extends Game {
     return true;
   }
 
-  /** method to insert correct letter in board.*/
+  /**
+   * Adds correctly guessed letter to character array of correctly guess letters
+   * @param c
+   *        the correctly guessed letter
+   */
   public void insertCorrectLetterGuess(char c) {
     for (int pos = 0; pos < getWordToGuess().length(); pos++) {
       if (wordToGuess.charAt(pos) == c) {
@@ -266,46 +418,76 @@ public class Hangman extends Game {
     }
   }
 
-  /** method to check if player has won.*/
+  /**
+   * Returns whether the player(s) has/have won.
+   *
+   * A player or players win(s) if their current guess is equal to the guess word, ignoring case,
+   * or if the character array of correctly guessed letters in string form is equal to the
+   * guess word, ignoring case.
+   * @return  {@code true} if player(s) has/have met win conditions; {@code false} otherwise
+   */
   public boolean hasPlayerWon() {
     return getWordToGuess().equalsIgnoreCase(currGuess)
             || getWordToGuess().equalsIgnoreCase(
                     StringManipulation.charArrayToString(getCorrectlyGuessedLetters()));
   }
 
-  /** method to check if player has lost.*/
+  /**
+   * Returns whether the player(s) has/have won.
+   *
+   * A player or players lose(s) if the number of incorrect guesses in the current round is
+   * equal to the number of allowed incorrect guesses.
+   * @return  {@code true} if player(s) has/have lost; {@code false} otherwise
+   */
   public boolean hasPlayerLost() {
     return numRoundIncorrectGuesses == NUM_ALLOWED_INCORRECT_GUESSES;
   }
 
-  /** method called if player has won.*/
+  /**
+   * Increments win streak by 1, increments number of rounds won by 1, prints win message,
+   * and adds 3 points to player's or players' score(s).
+   */
   public void playerWin() {
     incrementWinStreak();
     incrementNumRoundsWon();
     printWinMessage();
-    points += 3; //bonus for correctly guessing word
+    points += 3;
   }
 
-  /** method called if player has loss.*/
+  /**
+   * Resets win streak, increments number of rounds lost by 1, and prints loss message.
+   */
   public void playerLoss() {
     resetWinStreak();
     incrementNumRoundsLost();
     printLossMessage();
   }
 
-  /** method to check if round is finished.*/
+  /**
+   * Finishes the round and runs {@code playerWin} or {@code playerLoss} method, depending on
+   * whether player(s) won or lost.
+   *
+   * Prints the guess word, increments the number of rounds
+   * played by 1, and prints round statistics.
+   */
   public void roundFinish() {
     if (hasPlayerWon()) {
       playerWin();
     } else {
       playerLoss();
-      printGuessWord();
     }
+    printGuessWord();
     incrementNumRoundsPlayed();
     printRoundStats();
   }
 
-  /** new round method.*/
+  /**
+   * Starts a new round.
+   *
+   * Sets new random guess word, resets number of previous total, correct, and incorrect guesses
+   * and correctly guessedLetters, sets {@code firstGame} to false, indicating that this is at
+   * least the second game played, and starts a new round.
+   */
   public void newRound() {
     selectWordToGuess();
     resetGuesses();
@@ -314,33 +496,55 @@ public class Hangman extends Game {
     run();
   }
 
-  /** method to get the number of correct guesses.*/
+  /**
+   * Indicates that a letter has been correctly guessed.
+   *
+   * Increments number of total and correct guesses by 1, adds letter to correct letter guesses character
+   * array, and increments the player's or players' points by 1.
+   * @param guess
+   *        the character guessed
+   */
   public void correctGuess(char guess) {
     incrementNumTotalGuesses();
-    insertCorrectLetterGuess(guess);
     incrementNumCorrectGuesses();
     incrementPoints();
+    insertCorrectLetterGuess(guess);
   }
 
-  /** method to get the number of correct guesses when given as string.*/
+  /**
+   * Indicates that the guess word has been correctly guessed.
+   *
+   * Increments number of total guesses and correct guesses by 1.
+   * @param guess
+   *        the string guessed
+   */
   public void correctGuess(String guess) {
     incrementNumTotalGuesses();
     incrementNumCorrectGuesses();
   }
 
-  /** method to get the number of incorrect guesses.*/
+  /**
+   * Indicates that a guess was incorrect.
+   *
+   * Increments number of total and incorrect guesses by 1.
+   */
   public void incorrectGuess() {
     incrementNumTotalGuesses();
     incrementNumIncorrectGuesses();
   }
 
-  /** method to print how many guesses remaining after each turn.*/
+  /**
+   * Prints to player(s) how many incorrect guesses remain.
+   */
   public void printNumIncorrectGuessesRemaining() {
     printlnToAllPlayers("You have " + (NUM_ALLOWED_INCORRECT_GUESSES - numRoundIncorrectGuesses)
         + " incorrect guesses remaining.");
   }
 
-  /** method to print stats at end of the round.*/
+  /**
+   * Prints round-specific statistics including points, current win streak, and the number of total,
+   * correct, and incorrect guesses.
+   */
   public void printRoundStats() {
     printlnToAllPlayers("===== ROUND STATS ====="
         + "\nPoints: " + getPoints()
@@ -350,7 +554,9 @@ public class Hangman extends Game {
         + "\nTotal Incorrect Guesses: " + getNumRoundIncorrectGuesses());
   }
 
-  /** method to print stats when players finish game.*/
+  /**
+   * Prints game-specific statistics including number of total rounds played, won, and lost.
+   */
   public void printGameStats() {
     printlnToAllPlayers("===== GAME STATS ====="
         + "\nTotal # of Rounds Played: " + getNumRoundsPlayed()
@@ -358,42 +564,59 @@ public class Hangman extends Game {
         + "\nTotal # of Rounds Lost: " + getNumRoundsLost());
   }
 
-  /** prints win message.*/
+  /**
+   * Prints win message to player(s)
+   */
   public void printWinMessage() {
     printlnToAllPlayers(winMessage);
   }
 
-  /** prints loss message.*/
+  /**
+   * Prints loss message to player(s)
+   */
   public void printLossMessage() {
     printlnToAllPlayers(lossMessage);
   }
 
-  /** prints word when players loose.*/
+  /**
+   * Prints guess word to player(s)
+   */
   public void printGuessWord() {
     printlnToAllPlayers("The word was: " + getWordToGuess());
   }
 
-  /** prints invalid guesss.*/
+  /**
+   * Prints invalid guess message to player(s)
+   */
   public void printInvalidGuess() {
     System.out.println(invalidGuess);
   }
 
-  /** prints game instructions.*/
+  /**
+   * Prints instructions to player(s)
+   */
   public void printInstructions() {
     printlnToAllPlayers(instructions);
   }
 
-  /** prints welcome message.*/
+  /**
+   * Prints welcome message to player(s)
+   */
   public void printWelcomeMessage() {
     printlnToAllPlayers(welcomeMessage);
   }
 
-  /** method to print the updated board at the start of each round.*/
+  /**
+   * Displays progress of guess word to player(s)
+   */
   public void printGuessWordPrompt() {
     printlnToAllPlayers("Word: " + getGuessWordProgress());
   }
 
-  /** method to play one round of the game.*/
+  /**
+   * Plays through one round of Hangman game.
+   * @throws IOException
+   */
   public void tick() throws IOException {
     printNumIncorrectGuessesRemaining();
     printGuessWordPrompt();
@@ -403,6 +626,4 @@ public class Hangman extends Game {
     checkIfGuessCorrect(getCurrGuess());
     switchTurn();
   }
-
-  //TODO hint command "would you like a hint?"
 }
